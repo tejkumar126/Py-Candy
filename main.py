51716a0be62adfe8172bd7c55785aa42d9448f85
tejkumar126 <83543609+tejkumar126@users.noreply.github.com>
2022-04-16 11:31:58 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,524 @@
+#Imports Start
+import discord, subprocess, sys, time, os, colorama, codecs, datetime, random, datetime, smtplib, string, ctypes
+import urllib.request, re, json, requests, webbrowser, aiohttp, asyncio, functools, logging
+import discord
+from discord.ext import commands
+import asyncio
+import random
+import logging
+import datetime
+import time
+from bs4 import BeautifulSoup as bs4
+from selenium import webdriver
+from threading import Thread
+from subprocess import call 
+from itertools import cycle
+from colorama import Fore
+from sys import platform
+from PIL import Image
+import pyPrivnote as pn
+import discord
+from discord.ext import commands
+import os
+import random
+import asyncio
+import logging
+import datetime
+import time
+import random
+import discord
+from discord.ext import commands
+from discord_components import *
+import datetime
+from discord.ext import commands,tasks
+from discord.ext.commands import has_permissions, CheckFailure
+#Imports End
+
+#Prefix Client Start 
+client = discord.Client()
+client = commands.Bot(command_prefix= 'p!')
+print("Online")
+#Prefix Client End  
+
+
+#Status Start
+@client.event
+async def on_ready():
+    activity = discord.Game(name="PyCyber ", type=3)
+    await client.change_presence(status=discord.Status.dnd, activity=activity)
+#Status End 
+
+@client.command(aliases=["mc"])
+
+async def member_count(ctx):
+
+    a=ctx.guild.member_count
+    b=discord.Embed(title=f"Members in {ctx.guild.name}",description=a,color=discord.Color((0xffff00)))
+    await ctx.send(embed=b)
+
+
+#Ping Start
+@client.command()
+async def ping(ctx):
+     await ctx.send(f'Pong! In {round(client.latency * 100)}ms')
+#Ping End
+
+
+#Me Start
+@client.command(name="me")
+async def me(ctx):
+    await ctx.send(f"You are {ctx.message.author.name}")
+#Me End
+
+
+
+@client.command()
+async def slowmode(ctx, seconds: int):
+    await ctx.channel.edit(slowmode_delay=seconds)
+    await ctx.send(f"Set the slowmode delay in this channel to {seconds} seconds!")
+
+
+@client.command(aliases=['slots', 'bet'])
+async def slot(ctx): # b'\xfc'
+    emojis = "🍎🍊🍐🍋🍉🍇🍓🍒"
+    a = random.choice(emojis)
+    b = random.choice(emojis)
+    c = random.choice(emojis)
+    slotmachine = f"**[ {a} {b} {c} ]\n{ctx.author.name}**,"
+    if (a == b == c):
+        await ctx.send(embed=discord.Embed.from_dict({"title":"Slot Machine", "description":f"{slotmachine} All matchings, you won!"}))
+    elif (a == b) or (a == c) or (b == c):
+        await ctx.send(embed=discord.Embed.from_dict({"title":"Slot Machine", "description":f"{slotmachine} 2 in a row, you won!"}))
+    else:
+        await ctx.send(embed=discord.Embed.from_dict({"title":"Slot Machine", "description":f"{slotmachine} No match, you lost"}))
+
+@client.command()
+@has_permissions(manage_channels=True)
+async def lockdown(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role,send_messages=False)
+    await ctx.send( ctx.channel.mention + " ***is now in lockdown.***")
+
+@client.command()
+@has_permissions(manage_channels=True)
+async def unlock(ctx):
+    await ctx.channel.set_permissions(ctx.guild.default_role, send_messages=True)
+    await ctx.send(ctx.channel.mention + " ***has been unlocked.***")
+
+
+@client.command()
+async def joke(ctx):  # b'\xfc'
+    await ctx.message.delete()
+    headers = {
+        "Accept": "application/json"
+    }
+    async with aiohttp.clientSession()as session:
+        async with session.get("https://icanhazdadjoke.com", headers=headers) as req:
+            r = await req.json()
+    await ctx.send(r["joke"])
+
+
+
+@client.command()
+async def abc(ctx): # b'\xfc'
+    ABC = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'ñ', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    message = await ctx.send(ABC[0])
+    await asyncio.sleep(2)
+    for _next in ABC[1:]:
+        await message.edit(content=_next)
+        await asyncio.sleep(2)
+
+@client.command()
+async def hack(ctx, user: discord.Member): # b'\xfc'
+    ABC = ['•Finding Password•', '•Finding Ip•', '•Sending Ip Grabber•', '•Found Ip•', '•Taking Details•', '•Making Few Changes•', '•Destroying Account•', '•The Dangerous Hack Is Completed•']
+    message = await ctx.send(ABC[0])
+    await asyncio.sleep(2)
+    for _next in ABC[1:]:
+        await message.edit(content=_next)
+        await asyncio.sleep(2)
+
+
+@client.command()
+async def topic(ctx): 
+    await ctx.message.delete()
+    r = requests.get('https://www.conversationstarters.com/generator.php').content
+    soup = bs4(r, 'html.parser')
+    topic = soup.find(id="random").text
+    await ctx.send(topic)
+
+
+
+@client.command()
+async def pat(ctx, user: discord.Member): # b'\xfc'
+    r = requests.get("https://nekos.life/api/v2/img/pat")
+    res = r.json()
+    em = discord.Embed(description=user.mention)
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@client.command()
+async def kiss(ctx, user: discord.Member): # b'\xfc'
+    r = requests.get("https://nekos.life/api/v2/img/kiss")
+    res = r.json()
+    em = discord.Embed(description=user.mention)
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+
+
+@client.command()
+async def tickle(ctx, user: discord.Member): # b'\xfc'
+    r = requests.get("https://nekos.life/api/v2/img/tickle")
+    res = r.json()
+    em = discord.Embed(description=user.mention)
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@client.command()
+async def slap(ctx, user: discord.Member): # b'\xfc'
+    r = requests.get("https://nekos.life/api/v2/img/slap")
+    res = r.json()
+    em = discord.Embed(description=user.mention)
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@client.command()
+async def hug(ctx, user: discord.Member): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get("https://nekos.life/api/v2/img/hug")
+    res = r.json()
+    em = discord.Embed(description=user.mention)
+    em.set_image(url=res['url'])
+    await ctx.send(embed=em)
+
+@client.command()
+async def ship(ctx, name1, name2): # b'\xfc'
+    await ctx.message.delete()
+    name1letters = name1[:round(len(name1) / 2)]
+    name2letters = name2[round(len(name2) / 2):]
+    ship = "".join([name1letters, name2letters])
+    emb = (discord.Embed(description=f"{ship}"))
+    emb.set_author(name=f"{name1} + {name2}")
+    await ctx.send(embed=emb)     
+
+
+@client.command()
+async def tinyurl(ctx, *, link): # b'\xfc'
+    await ctx.message.delete()
+    r = requests.get(f'http://tinyurl.com/api-create.php?url={link}').text
+    em = discord.Embed()
+    em.add_field(name='Shortened link', value=r, inline=False )
+    await ctx.send(embed=em)
+
+
+
+@client.command()
+async def tweet(ctx, username: str, *, message: str): # b'\xfc'
+    await ctx.message.delete()
+    async with aiohttp.ClientSession() as cs:
+        async with cs.get(f"https://nekobot.xyz/api/imagegen?type=tweet&username={username}&text={message}") as r:
+            res = await r.json()
+            em = discord.Embed()
+            em.set_image(url=res["message"])
+            await ctx.send(embed=em)
+
+
+@client.command()
+async def bold(ctx, *, message): # b'\xfc'
+    await ctx.message.delete()
+    await ctx.send('**'+message+'**')
+
+
+@client.command()
+async def genname(ctx): # b'\xfc'
+    await ctx.message.delete()
+    first, second = random.choices(ctx.guild.members, k=2)
+    first = first.display_name[len(first.display_name) // 2:]
+    second = second.display_name[:len(second.display_name) // 2]
+    await ctx.send(discord.utils.escape_mentions(second + first))
+
+
+
+
+
+@client.event
+async def cal():
+    DiscordComponents(client)
+ 
+#buttons array
+buttons = [
+    [
+        Button(style=ButtonStyle.grey, label='1'),
+        Button(style=ButtonStyle.grey, label='2'),
+        Button(style=ButtonStyle.grey, label='3'),
+        Button(style=ButtonStyle.blue, label='×'),
+        Button(style=ButtonStyle.red, label='Exit')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='4'),
+        Button(style=ButtonStyle.grey, label='5'),
+        Button(style=ButtonStyle.grey, label='6'),
+        Button(style=ButtonStyle.blue, label='÷'),
+        Button(style=ButtonStyle.red, label='←')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='7'),
+        Button(style=ButtonStyle.grey, label='8'),
+        Button(style=ButtonStyle.grey, label='9'),
+        Button(style=ButtonStyle.blue, label='+'),
+        Button(style=ButtonStyle.red, label='Clear')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='00'),
+        Button(style=ButtonStyle.grey, label='0'),
+        Button(style=ButtonStyle.grey, label='.'),
+        Button(style=ButtonStyle.blue, label='-'),
+        Button(style=ButtonStyle.green, label='=')
+    ],
+]
+ 
+#calculates answer
+def calculate(exp):
+    o = exp.replace('×', '*')
+    o = o.replace('÷', '/')
+    result = ''
+    try:
+        result = str(eval(o))
+    except:
+        result = 'An error occurred.'
+    return result
+ 
+@client.event
+async def on_ready():
+	#turns on discord components lib
+    DiscordComponents(client)
+ 
+#buttons array
+buttons = [
+    [
+        Button(style=ButtonStyle.grey, label='1'),
+        Button(style=ButtonStyle.grey, label='2'),
+        Button(style=ButtonStyle.grey, label='3'),
+        Button(style=ButtonStyle.blue, label='×'),
+        Button(style=ButtonStyle.red, label='Exit')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='4'),
+        Button(style=ButtonStyle.grey, label='5'),
+        Button(style=ButtonStyle.grey, label='6'),
+        Button(style=ButtonStyle.blue, label='÷'),
+        Button(style=ButtonStyle.red, label='←')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='7'),
+        Button(style=ButtonStyle.grey, label='8'),
+        Button(style=ButtonStyle.grey, label='9'),
+        Button(style=ButtonStyle.blue, label='+'),
+        Button(style=ButtonStyle.red, label='Clear')
+    ],
+    [
+        Button(style=ButtonStyle.grey, label='00'),
+        Button(style=ButtonStyle.grey, label='0'),
+        Button(style=ButtonStyle.grey, label='.'),
+        Button(style=ButtonStyle.blue, label='-'),
+        Button(style=ButtonStyle.green, label='=')
+    ],
+]
+ 
+#calculates answer
+def calculate(exp):
+    o = exp.replace('×', '*')
+    o = o.replace('÷', '/')
+    result = ''
+    try:
+        result = str(eval(o))
+    except:
+        result = 'An error occurred.'
+    return result
+ 
+@client.command()
+async def calc(ctx):
+    m = await ctx.send(content='Loading Calculators...')
+    expression = 'None'
+    delta = datetime.datetime.utcnow() + datetime.timedelta(minutes=5)
+    e = discord.Embed(title=f'{ctx.author.name}\'s calculator | {ctx.author.id}', description=expression,
+                        timestamp=delta)
+    await m.edit(components=buttons, embed=e)
+    while m.created_at < delta:
+        res = await client.wait_for('button_click')
+        if res.author.id == int(res.message.embeds[0].title.split('|')[1]) and res.message.embeds[
+            0].timestamp < delta:
+            expression = res.message.embeds[0].description
+            if expression == 'None' or expression == 'An error occurred.':
+                expression = ''
+            if res.component.label == 'Exit':
+                await res.respond(content='Calculator Closed', type=7)
+                break
+            elif res.component.label == '←':
+                expression = expression[:-1]
+            elif res.component.label == 'Clear':
+                expression = 'None'
+            elif res.component.label == '=':
+                expression = calculate(expression)
+            else:
+                expression += res.component.label
+            f = discord.Embed(title=f'{res.author.name}\'s calculator|{res.author.id}', description=expression,
+                                timestamp=delta)
+            await res.respond(content='', embed=f, components=buttons, type=7)
+ 
+ 
+
+
+player1 = ""
+player2 = ""
+turn = ""
+gameOver = True
+
+board = []
+
+winningConditions = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+]
+
+@client.command()
+async def tictactoe(ctx, p1: discord.Member, p2: discord.Member):
+    global count
+    global player1
+    global player2
+    global turn
+    global gameOver
+
+    if gameOver:
+        global board
+        board = [":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:",
+                 ":white_large_square:", ":white_large_square:", ":white_large_square:"]
+        turn = ""
+        gameOver = False
+        count = 0
+
+        player1 = p1
+        player2 = p2
+
+        # print the board
+        line = ""
+        for x in range(len(board)):
+            if x == 2 or x == 5 or x == 8:
+                line += " " + board[x]
+                await ctx.send(line)
+                line = ""
+            else:
+                line += " " + board[x]
+
+        # determine who goes first
+        num = random.randint(1, 2)
+        if num == 1:
+            turn = player1
+            await ctx.send("It is <@" + str(player1.id) + ">'s turn.")
+        elif num == 2:
+            turn = player2
+            await ctx.send("It is <@" + str(player2.id) + ">'s turn.")
+    else:
+        await ctx.send("A game is already in progress! Finish it before starting a new one.")
+
+@client.command()
+async def place(ctx, pos: int):
+    global turn
+    global player1
+    global player2
+    global board
+    global count
+    global gameOver
+
+    if not gameOver:
+        mark = ""
+        if turn == ctx.author:
+            if turn == player1:
+                mark = ":regional_indicator_x:"
+            elif turn == player2:
+                mark = ":o2:"
+            if 0 < pos < 10 and board[pos - 1] == ":white_large_square:" :
+                board[pos - 1] = mark
+                count += 1
+
+                # print the board
+                line = ""
+                for x in range(len(board)):
+                    if x == 2 or x == 5 or x == 8:
+                        line += " " + board[x]
+                        await ctx.send(line)
+                        line = ""
+                    else:
+                        line += " " + board[x]
+
+                checkWinner(winningConditions, mark)
+                print(count)
+                if gameOver == True:
+                    await ctx.send(mark + " wins!")
+                elif count >= 9:
+                    gameOver = True
+                    await ctx.send("It's a tie!")
+
+                # switch turns
+                if turn == player1:
+                    turn = player2
+                elif turn == player2:
+                    turn = player1
+            else:
+                await ctx.send("Be sure to choose an integer between 1 and 9 (inclusive) and an unmarked tile.")
+        else:
+            await ctx.send("It is not your turn.")
+    else:
+        await ctx.send("Please start a new game using the !tictactoe command.")
+
+
+def checkWinner(winningConditions, mark):
+    global gameOver
+    for condition in winningConditions:
+        if board[condition[0]] == mark and board[condition[1]] == mark and board[condition[2]] == mark:
+            gameOver = True
+
+@tictactoe.error
+async def tictactoe_error(ctx, error):
+    print(error)
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please mention 2 players for this command.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to mention/ping players (ie. @.")
+
+@place.error
+async def place_error(ctx, error):
+    if isinstance(error, commands.MissingRequiredArgument):
+        await ctx.send("Please enter a position you would like to mark.")
+    elif isinstance(error, commands.BadArgument):
+        await ctx.send("Please make sure to enter an integer.")
+
+
+@client.command()
+async def ban(ctx, user: discord.Member, *, reason):
+	await user.ban(reason=reason)
+
+
+@client.command()
+async def unban(ctx, user: discord.Member, *, reason):
+	await user.unban(reason=reason)
+
+
+@client.command()
+async def id(ctx, member: discord.Member):
+	await ctx.send(f'User id is {member.id}')
+
+
+@client.command(pass_context=True)
+async def nick(ctx, member: discord.Member, nick):
+	await member.edit(nick=nick)
+	await ctx.send(f'Nickname was changed for {member.mention} ')
+
+
+client.run('')
